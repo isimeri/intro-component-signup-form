b73dc68f18d4186225a72d9769eb093fa001085c
Isimeri Tori <jake.yogg.saron@gmail.com>
2020-11-25 07:11:42 +0200

    implemented the red exclamation button thing, desktop version done

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const lname = form.querySelector(".last-name");
 const password = form.querySelector(".password-input");
 const email = form.querySelector(".email-input");
 
+
 fname.addEventListener('input', function(){
     if(fname.validity.valid)
     {
         fname.classList.remove("error");
-        fname.nextElementSibling.classList.remove("show");
+        fname.parentElement.classList.remove("goodnt");
+        fname.parentElement.nextElementSibling.classList.remove("show");
     }
 });
 
@@ -16,7 +18,8 @@ lname.addEventListener('input', function(){
     if(lname.validity.valid)
     {
         lname.classList.remove("error");
-        lname.nextElementSibling.classList.remove("show");
+        lname.parentElement.classList.remove("goodnt");
+        lname.parentElement.nextElementSibling.classList.remove("show");
     }
 });
 
@@ -24,7 +27,8 @@ email.addEventListener('input', function(){
     if(email.validity.valid)
     {
         email.classList.remove("error");
-        email.nextElementSibling.classList.remove("show");
+        email.parentElement.classList.remove("goodnt");
+        email.parentElement.nextElementSibling.classList.remove("show");
     }
 });
 
@@ -32,7 +36,8 @@ password.addEventListener('input', function(){
     if(password.validity.valid)
     {
         password.classList.remove("error");
-        password.nextElementSibling.classList.remove("show");
+        password.parentElement.classList.remove("goodnt");
+        password.parentElement.nextElementSibling.classList.remove("show");
     }
 });
 
@@ -40,25 +45,43 @@ form.addEventListener('submit', function(e){
     if(!fname.validity.valid)
     {
         fname.classList.add("error");
-        fname.nextElementSibling.classList.add("show");
+        fname.parentElement.classList.add("goodnt");
+        fname.parentElement.nextElementSibling.classList.add("show");
         e.preventDefault();
     }
     if(!lname.validity.valid)
     {
         lname.classList.add("error");
-        lname.nextElementSibling.classList.add("show");
+        lname.parentElement.classList.add("goodnt");
+        lname.parentElement.nextElementSibling.classList.add("show");
         e.preventDefault();
     }
     if(!email.validity.valid)
     {
         email.classList.add("error");
-        email.nextElementSibling.classList.add("show");
+        email.parentElement.classList.add("goodnt");
+        email.parentElement.nextElementSibling.classList.add("show");
         e.preventDefault();
     }
     if(!password.validity.valid)
     {
         password.classList.add("error");
-        password.nextElementSibling.classList.add("show");
+        password.parentElement.classList.add("goodnt");
+        password.parentElement.nextElementSibling.classList.add("show");
         e.preventDefault();
     }
+    if(fname.validity.valid && lname.validity.valid && email.validity.valid && password.validity.valid)
+    {
+        alert("You got a free trial, cool.");
+        e.preventDefault();
+        cleanUp();
+        return;
+    }
+    
 });
+function cleanUp(){
+    fname.value = "";
+    lname.value = "";
+    email.value = "";
+    password.value = "";
+}
